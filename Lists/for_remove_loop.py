296bf211d90495d21b04a0eda7fdd41c1f2a3d2d
k4kaki <k4kaki@gmail.com>
2018-03-22 00:00:46 +0530

    some more python files have been added

---
+++ Lists/for_remove_loop.py
@@ -0,0 +1,11 @@
+def my_fun(m):
+    x = m[:]
+    for k in x:
+        print(type(k))
+        if type(k) == int:
+            
+            m.remove(k)
+
+values = [3, [3,4], 2.9, 3, 6, 'dog', 5]
+my_fun(values)
+print(values)
