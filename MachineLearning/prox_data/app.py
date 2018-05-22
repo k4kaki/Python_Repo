87f2531ee4cb0a84136fb1e4c4119434bb1947af
root <root@ip-172-31-21-28.us-west-2.compute.internal>
2018-05-22 09:36:55 +0000

    working copy of app

--- MachineLearning/prox_data/app.py
+++ MachineLearning/prox_data/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request
 from werkzeug import secure_filename
 import xlrd
-import pymongo
+from collections import OrderedDict
+import datetime
+from pymongo import MongoClient
 import simplejson as json
 app = Flask(__name__)
 
@@ -14,7 +16,7 @@ def uploader():
    if request.method == 'POST':
       f = request.files['file']
       f.save(secure_filename(f.filename))
-      wb = xlrd.open_workbook("f.filename")
+      wb = xlrd.open_workbook(f.filename)
       sh = wb.sheet_by_index(0)
       order_list = []
       keys = [sh.cell(0, col_index).value for col_index in range(sh.ncols)]     
@@ -22,10 +24,27 @@ def uploader():
           orders = OrderedDict()
           row_values = sh.row_values(rownum)
           orders["ID"] = sh.cell(rownum,0).value
-          orders.update({keys[col_index]: [{"status":sh.cell(rownum, col_index).value,"status_date":"2017-05-18"}] for col_index in range(1,sh.ncols)})
+          orders.update({keys[col_index]: [{"status":sh.cell(rownum, col_index).value,"status_date":datetime.datetime.utcnow().strftime('%Y-%m-%d')}] for col_index in range(1,sh.ncols)})
           order_list.append(orders)
-     j = json.dumps(order_list)
-     with open('sample.json','w') as f:
+      j = json.dumps(order_list)
+      with open('sample.json','w') as f:
           f.write(j)
+
+      client = MongoClient('34.219.47.108',27017)
+      pxsdb=client.sample_DB
+      current_json=json.loads(open('sample.json').read())
+      order_id=pxsdb.existing_order_tbl.distinct("ID")
+      for json_dict in current_json:
+          if json_dict["ID"] in order_id:
+              for key in json_dict:
+                  if key != "ID":
+                      if pxsdb.existing_order_tbl.find_one({"ID":json_dict['ID']})[key][-1]["status"] != json_dict[key][0]["status"]:
+                          pxsdb.existing_order_tbl.update({'_id' :pxsdb.existing_order_tbl.find_one({"ID":json_dict['ID']})['_id']}, {'$addToSet' :{key  : {"status":json_dict[key][0]["status"],"status_date":"2020-01-01"}}})
+                      else:
+                          print("Same status")
+          else:
+              pxsdb.existing_order_tbl.insert(json_dict)
+
+
 if __name__ == '__main__':
    app.run(debug = True)
