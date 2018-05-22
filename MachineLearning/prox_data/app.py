eea7f746f5fcdc5d66834f31d10590b0c5abd84c
root <root@ip-172-31-21-28.us-west-2.compute.internal>
2018-05-22 10:50:48 +0000

    Try and exception block added

--- MachineLearning/prox_data/app.py
+++ MachineLearning/prox_data/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from werkzeug import secure_filename
 import xlrd
+from xlrd import XLRDError
 from collections import OrderedDict
 import datetime
 from pymongo import MongoClient
@@ -16,20 +17,22 @@ def uploader():
    if request.method == 'POST':
       f = request.files['file']
       f.save(secure_filename(f.filename))
-      wb = xlrd.open_workbook(f.filename)
-      sh = wb.sheet_by_index(0)
-      order_list = []
-      keys = [sh.cell(0, col_index).value for col_index in range(sh.ncols)]     
-      for rownum in range(1, sh.nrows):
-          orders = OrderedDict()
-          row_values = sh.row_values(rownum)
-          orders["ID"] = sh.cell(rownum,0).value
-          orders.update({keys[col_index]: [{"status":sh.cell(rownum, col_index).value,"status_date":datetime.datetime.utcnow().strftime('%Y-%m-%d')}] for col_index in range(1,sh.ncols)})
-          order_list.append(orders)
-      j = json.dumps(order_list)
-      with open('sample.json','w') as f:
-          f.write(j)
-
+      try:
+          wb = xlrd.open_workbook(f.filename)
+          sh = wb.sheet_by_index(0)
+          order_list = []
+          keys = [sh.cell(0, col_index).value for col_index in range(sh.ncols)]     
+          for rownum in range(1, sh.nrows):
+              orders = OrderedDict()
+              row_values = sh.row_values(rownum)
+              orders["ID"] = sh.cell(rownum,0).value
+              orders.update({keys[col_index]: [{"status":sh.cell(rownum, col_index).value,"status_date":datetime.datetime.utcnow().strftime('%Y-%m-%d')}] for col_index in range(1,sh.ncols)})
+              order_list.append(orders)
+          j = json.dumps(order_list)
+          with open('sample.json','w') as f:
+              f.write(j)
+      except XLRDError as e:
+          return "The format of the file is not LEGAL"
       client = MongoClient('34.219.47.108',27017)
       pxsdb=client.sample_DB
       current_json=json.loads(open('sample.json').read())
